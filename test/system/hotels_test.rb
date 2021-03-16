30425699cac2d8d49c6b87d1cec96ca914da50f3
ngani faith <nganifaith@gmail.com>
2021-03-16 12:14:19 +0100

    fix errors

--- test/system/hotels_test.rb
+++ test/system/hotels_test.rb
@@ -1,49 +1,49 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class HotelsTest < ApplicationSystemTestCase
   setup do
     @hotel = hotels(:one)
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit hotels_url
-    assert_selector "h1", text: "Hotels"
+    assert_selector 'h1', text: 'Hotels'
   end
 
-  test "creating a Hotel" do
+  test 'creating a Hotel' do
     visit hotels_url
-    click_on "New Hotel"
+    click_on 'New Hotel'
 
-    fill_in "Address", with: @hotel.address
-    fill_in "Country", with: @hotel.country
-    fill_in "Name", with: @hotel.name
-    fill_in "Phone", with: @hotel.phone
-    click_on "Create Hotel"
+    fill_in 'Address', with: @hotel.address
+    fill_in 'Country', with: @hotel.country
+    fill_in 'Name', with: @hotel.name
+    fill_in 'Phone', with: @hotel.phone
+    click_on 'Create Hotel'
 
-    assert_text "Hotel was successfully created"
-    click_on "Back"
+    assert_text 'Hotel was successfully created'
+    click_on 'Back'
   end
 
-  test "updating a Hotel" do
+  test 'updating a Hotel' do
     visit hotels_url
-    click_on "Edit", match: :first
+    click_on 'Edit', match: :first
 
-    fill_in "Address", with: @hotel.address
-    fill_in "Country", with: @hotel.country
-    fill_in "Name", with: @hotel.name
-    fill_in "Phone", with: @hotel.phone
-    click_on "Update Hotel"
+    fill_in 'Address', with: @hotel.address
+    fill_in 'Country', with: @hotel.country
+    fill_in 'Name', with: @hotel.name
+    fill_in 'Phone', with: @hotel.phone
+    click_on 'Update Hotel'
 
-    assert_text "Hotel was successfully updated"
-    click_on "Back"
+    assert_text 'Hotel was successfully updated'
+    click_on 'Back'
   end
 
-  test "destroying a Hotel" do
+  test 'destroying a Hotel' do
     visit hotels_url
     page.accept_confirm do
-      click_on "Destroy", match: :first
+      click_on 'Destroy', match: :first
     end
 
-    assert_text "Hotel was successfully destroyed"
+    assert_text 'Hotel was successfully destroyed'
   end
 end
